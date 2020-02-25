c3873ca6cd6011af652e1bd20864011e02353284
ancilt26 <ancilthomas26@gmail.com>
2020-02-25 12:40:25 +0800

    Update greeting message

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3001;
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3001
 
-app.get("/", (req, res) => res.send("Hello from Render!"));
+app.get('/', (req, res) => res.send('Hello from BuyerUp!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
